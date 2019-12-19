cb6635a4171e47e1a61d0a36daa1200e130afb90
brandongatlin <brandongatlin1981@me.com>
2019-12-18 23:11:24 -0600

    added methods mark for writing and nudge for yes or no confirm

--- 04-cli-application/cliApp.js
+++ 04-cli-application/cliApp.js
@@ -10,11 +10,14 @@ const questions = [
 async function main() {
     const sniffer = new Hound(questions);
 
-    const answers = await sniffer.sniff();
+    // const answers = await sniffer.sniff();
 
-    answers.forEach((answer, i) => {
-        console.log(`${questions[i]} answered with ${answer}`);
-    });
+    // answers.forEach((answer, i) => {
+    //     sniffer.mark(`${questions[i]} answered with ${answer}\n`)
+    // });
+
+    const confirm = await sniffer.nudge("did you eat tacos today?");
+    sniffer.mark(confirm);
 
     sniffer.endSniff();
 }
--- 04-cli-application/hound.js
+++ 04-cli-application/hound.js
@@ -4,20 +4,18 @@ const rl = readline.createInterface({
     terminal: true,
     input: process.stdin,
     output: process.stdout,
+    prompt: '=> '
 });
 
-const Hound = function Hound(questions=[]) {
-    if(!Array.isArray(questions)) throw new Error("Wrong Type: Questions should be an array");
+const Hound = function Hound(questions = []) {
+    if (!Array.isArray(questions)) throw new Error("Wrong Type: Questions should be an array");
     this.questions = questions;
 }
 
-// Walk through all questions
-// Get all the answers 
-// Return all the answers
-
 Hound.prototype.askQuestion = function askQuestion(question) {
+
     return new Promise((resolve, reject) => {
-        rl.question(`${question} >> `, (answer) => {
+        rl.question(`\n${question} >> `, (answer) => {
             resolve(answer);
         });
     });
@@ -25,12 +23,41 @@ Hound.prototype.askQuestion = function askQuestion(question) {
 
 Hound.prototype.sniff = async function sniff() {
     const answers = [];
-    for(const question of this.questions) {
+    for (const question of this.questions) {
         const answer = await this.askQuestion(question);
+        const pos = rl.getCursorPos();
+        const cur = rl.cursor;
+        console.log(pos, cur);
         answers.push(answer);
     }
 
     return answers;
+}
+
+Hound.prototype.nudge = async function nudge(question) {
+    const options = ['y', 'yes', 'n', 'no'];
+    let answer;
+
+    while (true) {
+        answer = await this.askQuestion(`${question} [Y/n]`);
+
+        if (answer === '') {
+            answer = 'yes';
+        }
+
+        answer = answer.toLowerCase();
+
+        if (!options.includes(answer)) {
+            console.log('bad answer, dude...');
+        } else {
+            break;
+        }
+    }
+    return answer;
+}
+
+Hound.prototype.mark = async function mark(input) {
+    rl.write(input);
 }
 
 Hound.prototype.endSniff = function endSniff() {
